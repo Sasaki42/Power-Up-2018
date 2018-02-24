c6c5cd270254713097cb8f8811ec9b6578881eba
Sasaki42 <tylersasaki42@gmail.com>
2018-02-24 15:16:06 -0800

    Cleaned up even more
    The nested stuff is so much easier to read now and also lined up the variables

--- Robot.cpp
+++ Robot.cpp
@@ -43,30 +43,30 @@ class PowerUpRobot : public IterativeRobot {
 //Robot Constants
 //---------------------------------------------------------
 
-		static const uint JS_PORT_LEFT           =  0;
-		static const uint JS_PORT_RIGHT			 =  1;
-		static const uint CCI_PORT1        	     =  2;
+		static const uint JS_PORT_LEFT         		  =  0;
+		static const uint JS_PORT_RIGHT	       		  =  1;
+		static const uint CCI_PORT1        		  =  2;
 
 		// Joystick Button (right joystick)
-		static const uint CAMERA_LIGHT_SW_CH     =  1;
+		static const uint CAMERA_LIGHT_SW_CH     	  =  1;
 
 		// Driver Station CCI1 Channels (Uses joystick button references)
-		static const uint Intake_SW_CH			 =  6;
-		static const uint Outtake_SW_CH			 =  5;
-		static const uint AIROUT_SW_CH 			 =  1;
-		static const uint AIRIN_SW_CH			 =  2;
+		static const uint Intake_SW_CH			  =  6;
+		static const uint Outtake_SW_CH			  =  5;
+		static const uint AIROUT_SW_CH 			  =  1;
+		static const uint AIRIN_SW_CH			  =  2;
 
 		// roboRio PWM Channels (PWM = Pulse width modulation)
-		static const uint RIGHT_FRONT_MOTOR_CH	  =  9;
-		static const uint RIGHT_REAR_MOTOR_CH     =  8;
-		static const uint LEFT_FRONT_MOTOR_CH	  =  3;
-		static const uint LEFT_REAR_MOTOR_CH	  =  2;
-		static const uint LEFT_INTAKE_MOTOR_CH	  =  0;
-		static const uint RIGHT_INTAKE_MOTOR_CH	  =  1;
+		static const uint RIGHT_FRONT_MOTOR_CH	  	  =  9;
+		static const uint RIGHT_REAR_MOTOR_CH    	  =  8;
+		static const uint LEFT_FRONT_MOTOR_CH		  =  3;
+		static const uint LEFT_REAR_MOTOR_CH	 	  =  2;
+		static const uint LEFT_INTAKE_MOTOR_CH	   	  =  0;
+		static const uint RIGHT_INTAKE_MOTOR_CH	 	  =  1;
 		static const uint CLIMBER_MOTOR1_CH		  =  6;
 		static const uint CLIMBER_MOTOR2_CH		  =  7;
-		static const uint ELEVATOR_MOTOR1_CH 	  =  4;
-		static const uint ELEVATOR_MOTOR2_CH	  =  5;
+		static const uint ELEVATOR_MOTOR1_CH 	  	  =  4;
+		static const uint ELEVATOR_MOTOR2_CH	  	  =  5;
 
 		// roboRio Solenoid Channels
 		static const uint Compressor_CH 		  =  0;
@@ -82,22 +82,22 @@ class PowerUpRobot : public IterativeRobot {
 //---------------------------------------------------------
 
 		// Auto Drive speed
-		const float Speed_Drive_Auto    	      =	 0.5;
+		const float Speed_Drive_Auto    	          =  0.5;
 		const float Speed_Elevator_Auto			  =  0.5;
 
 		// Auto Drive Time
-		static const uint Time_Foward_Side1	      =  200;
-		static const uint Time_Foward_Side2	      =  300;
-		static const uint Time_Foward_ToNull  	  =  500;
-		static const uint Time_Foward_Center      =  200;
-		static const uint Time_Turn_Center	      =  210;
-		static const uint Time_Turn_Side          =  230;
+		static const uint Time_Foward_Side1	     	  =  200;
+		static const uint Time_Foward_Side2	     	  =  300;
+		static const uint Time_Foward_ToNull  	  	  =  500;
+		static const uint Time_Foward_Center      	  =  200;
+		static const uint Time_Turn_Center	    	  =  210;
+		static const uint Time_Turn_Side         	  =  230;
 		static const uint Time_Elevator		  	  =  250;
 
 		// Delay value
-		static const uint Time_Elevator_Delay	  =  50;
-		bool			   Delayed			      = false;
-		bool 			   DelayTriggered 		  = false;
+		static const uint Time_Elevator_Delay	 	  =  50;
+		bool			   Delayed		  = false;
+		bool 			   DelayTriggered  	  = false;
 
 		//Starting positions
 		enum autoModeStates {kAM1Of};
@@ -179,35 +179,35 @@ START_ROBOT_CLASS(PowerUpRobot)
 PowerUpRobot::PowerUpRobot()
 {
 //joysticks & XBox
-	pDriveStickLeft		   = new Joystick(JS_PORT_LEFT);
-	pDriveStickRight	   = new Joystick(JS_PORT_RIGHT);
-	pXBox                  = new Joystick(CCI_PORT1);
+	pDriveStickLeft		  	   =  new Joystick(JS_PORT_LEFT);
+	pDriveStickRight	  	   =  new Joystick(JS_PORT_RIGHT);
+	pXBox                 		   =  new Joystick(CCI_PORT1);
 
-	pIntakeButton 		   = new JoystickButton(pXBox,Intake_SW_CH);
-	pOuttakeButton 		   = new JoystickButton(pXBox,Outtake_SW_CH);
-	pAirOut				   = new JoystickButton(pXBox,AIRIN_SW_CH);
-	pAirIn				   = new JoystickButton(pXBox,AIROUT_SW_CH);
-	pCompressor 		   = new Compressor(Compressor_CH);
+	pIntakeButton 		 	   =  new JoystickButton(pXBox,Intake_SW_CH);
+	pOuttakeButton 		  	   =  new JoystickButton(pXBox,Outtake_SW_CH);
+	pAirOut				   =  new JoystickButton(pXBox,AIRIN_SW_CH);
+	pAirIn				   =  new JoystickButton(pXBox,AIROUT_SW_CH);
+	pCompressor 		   	   =  new Compressor(Compressor_CH);
 
 //Mechanism pointer initialization
-	pDriveTrain		     = new RobotDrive(LEFT_FRONT_MOTOR_CH,LEFT_REAR_MOTOR_CH,RIGHT_FRONT_MOTOR_CH, RIGHT_REAR_MOTOR_CH);
-	pClimber		     = new Climber(CLIMBER_MOTOR1_CH, CLIMBER_MOTOR2_CH);
-	pIntake 			 = new Intake(LEFT_INTAKE_MOTOR_CH, RIGHT_INTAKE_MOTOR_CH);
-	pElevator 			 = new Elevator(ELEVATOR_MOTOR1_CH, ELEVATOR_MOTOR2_CH);
+	pDriveTrain		           =  new RobotDrive(LEFT_FRONT_MOTOR_CH,LEFT_REAR_MOTOR_CH,RIGHT_FRONT_MOTOR_CH, RIGHT_REAR_MOTOR_CH);
+	pClimber		   	   =  new Climber(CLIMBER_MOTOR1_CH, CLIMBER_MOTOR2_CH);
+	pIntake 			   =  new Intake(LEFT_INTAKE_MOTOR_CH, RIGHT_INTAKE_MOTOR_CH);
+	pElevator 			   =  new Elevator(ELEVATOR_MOTOR1_CH, ELEVATOR_MOTOR2_CH);
 
 //Variable initializations
-	loopCount      		= 0;
-	DelayCounter		= 0;
+	loopCount      			   =  0;
+	DelayCounter			   =  0;
 
-	rightDriveSpeed       = 0.0;
-	leftDriveSpeed        = 0.0;
+	rightDriveSpeed      		   =  0.0;
+	leftDriveSpeed      		   =  0.0;
 
-	LeftY				  = 0.0;
-	RightY				  = 0.0;
-	LeftTrigger  		  = 0.0;
-	RightTrigger		  = 0.0;
+	LeftY				   =  0.0;
+	RightY				   =  0.0;
+	LeftTrigger  		 	   =  0.0;
+	RightTrigger		 	   =  0.0;
 
-	Starting_Position     = 1;
+	Starting_Position    		   =  1;
 	return;
 }
 
@@ -225,24 +225,24 @@ void PowerUpRobot::RobotInit()
 
 void PowerUpRobot::DisabledInit()
 {
-	loopCount  = 0;
+	loopCount 			   =  0;
 	AirOut();
 	return;
 }
 
 void PowerUpRobot::AutonomousInit()
 {
-	loopCount  = 0;
-	Starting_Position = frc::DriverStation::GetInstance().GetLocation();
-	gameData = "L";
+	loopCount  			   =  0;
+	Starting_Position 		   =  frc::DriverStation::GetInstance().GetLocation();
+	gameData 			   =  "L";
 	Solenoid1.Set(frc::DoubleSolenoid::Value::kReverse);
 	return;
 }
 
 void PowerUpRobot::TeleopInit()
 {
-	loopCount      = 0;
-	DelayCounter   = 0;
+	loopCount      			   =  0;
+	DelayCounter  			   =  0;
 	Solenoid1.Set(frc::DoubleSolenoid::Value::kForward);
 	pCompressor->SetClosedLoopControl(true);
 	return;
@@ -266,8 +266,8 @@ void PowerUpRobot::AutonomousPeriodic()
 	SmartDashboard::PutNumber("LoopCount",loopCount);
 	}
 	else{
-		Starting_Position = frc::DriverStation::GetInstance().GetLocation();
-		gameData = frc::DriverStation::GetInstance().GetGameSpecificMessage();
+		Starting_Position 	   =  frc::DriverStation::GetInstance().GetLocation();
+		gameData 		   =  frc::DriverStation::GetInstance().GetGameSpecificMessage();
 	}
 	return;
 }
@@ -278,10 +278,10 @@ void PowerUpRobot::TeleopPeriodic()
 	DelayCounter++;
 
 	GetDriverStationInput();
-    RunClimber();
-    RunIntake();
-    RunElevator();
-    SolenoidUpdate();
+    	RunClimber();
+   	RunIntake();
+   	RunElevator();
+   	SolenoidUpdate();
 
 	pDriveTrain->TankDrive(-leftDriveSpeed,-rightDriveSpeed);
 	return;
@@ -289,13 +289,13 @@ void PowerUpRobot::TeleopPeriodic()
 
 void PowerUpRobot::GetDriverStationInput()
 {
-	rightDriveSpeed		=  pDriveStickRight->GetY();
-	leftDriveSpeed		=  pDriveStickLeft->GetY();
+	rightDriveSpeed			   =  pDriveStickRight->GetY();
+	leftDriveSpeed			   =  pDriveStickLeft->GetY();
 
-	LeftY               = pXBox->GetRawAxis(1);
-	RightY              = pXBox->GetRawAxis(5);
-	LeftTrigger         = pXBox->GetRawAxis(2);
-	RightTrigger        = pXBox->GetRawAxis(3);
+	LeftY              		   =  pXBox->GetRawAxis(1);
+	RightY            		   =  pXBox->GetRawAxis(5);
+	LeftTrigger      		   =  pXBox->GetRawAxis(2);
+	RightTrigger        		   =  pXBox->GetRawAxis(3);
 
 #ifdef CONSOLE
     ShowDSValues();
@@ -325,17 +325,17 @@ void PowerUpRobot::AutoSwitch(){
 					}
 					else if(gameData[0]=='R'){
 						// go to null
-							if (loopCount < Time_Foward_ToNull) {
-								pDriveTrain->TankDrive(Speed_Drive_Auto,Speed_Drive_Auto);
-							}
-							else{
+						if (loopCount < Time_Foward_ToNull) {
+							pDriveTrain->TankDrive(Speed_Drive_Auto,Speed_Drive_Auto);
+						}
+						else{
 
-								}
-								}
-								else{
+						}
+					}
+					else{
 
-								}
-								break;
+					}
+					break;
 				case 2: 		//CENTER field
 					if(gameData[0]=='L'){
 						// go left
@@ -345,25 +345,25 @@ void PowerUpRobot::AutoSwitch(){
 						else if(loopCount < Time_Foward_Center){
 						}
 						else{
-						AutoCubeCenter();
+							AutoCubeCenter();
 						}
-						}
-						else if(gameData[0]=='R'){
+					}
+					else if(gameData[0]=='R'){
 							// go right
-							if (loopCount < Time_Turn_Center) {
-								pDriveTrain->TankDrive(Speed_Drive_Auto,-Speed_Drive_Auto);
-							}
-							else if(loopCount < Time_Foward_Center){
-								pDriveTrain->TankDrive(Speed_Drive_Auto,Speed_Drive_Auto);
-							}
-							else{
-								AutoCubeCenter();
-								}
-							}
-							else{
+						if (loopCount < Time_Turn_Center) {
+							pDriveTrain->TankDrive(Speed_Drive_Auto,-Speed_Drive_Auto);
+						}
+						else if(loopCount < Time_Foward_Center){
+							pDriveTrain->TankDrive(Speed_Drive_Auto,Speed_Drive_Auto);
+						}
+						else{
+							AutoCubeCenter();
+						}
+					}
+					else{
 
-							}
-							break;
+					}
+					break;
 				case 3: 	     	//RIGHT Side
 					if(gameData[0]=='L'){
 						//go to null
@@ -372,15 +372,15 @@ void PowerUpRobot::AutoSwitch(){
 						}
 						else{
 						}
-						}
-						else if(gameData[0]=='R'){
-							//put cube on switch
-							AutoCubeSide();
-						}
-						else{
-						}
-						break;
-						}
+					}
+					else if(gameData[0]=='R'){
+						//put cube on switch
+						AutoCubeSide();
+					}
+					else{
+					}
+					break;
+				}
 	return;
 }
 
@@ -388,34 +388,34 @@ void PowerUpRobot::AutoCubeSide(){
 	int Speed_Auto_Turn;
 	// assigns direction for turning based on location
 	if(Starting_Position==1){
-			Speed_Auto_Turn		=	Speed_Drive_Auto;
+		Speed_Auto_Turn	   =  Speed_Drive_Auto;
 	}
 	else{
-			Speed_Auto_Turn		=	-Speed_Drive_Auto;
+		Speed_Auto_Turn	   =  -Speed_Drive_Auto;
 	}
 
 	if(loopCount<Time_Foward_Side1){
 		pDriveTrain->TankDrive(Speed_Drive_Auto,Speed_Drive_Auto);
 	}
 	else{
-	if(loopCount<Time_Turn_Side){
-		pDriveTrain->TankDrive(Speed_Auto_Turn,-Speed_Auto_Turn);
-	}
-	else{
-	if(loopCount<Time_Foward_Side2){
-		pDriveTrain->TankDrive(Speed_Drive_Auto,Speed_Drive_Auto);
+		if(loopCount<Time_Turn_Side){
+			pDriveTrain->TankDrive(Speed_Auto_Turn,-Speed_Auto_Turn);
 		}
 		else{
-		if(loopCount<Time_Elevator){
-			pElevator->Up1(Speed_Elevator_Auto);
-		}
-		else{
-			pElevator->Stop();
-			Stage1Stop();
-			AirOut();
-			pIntake->Out();
-		}
-		}
+			if(loopCount<Time_Foward_Side2){
+				pDriveTrain->TankDrive(Speed_Drive_Auto,Speed_Drive_Auto);
+			}
+			else{
+				if(loopCount<Time_Elevator){
+					pElevator->Up1(Speed_Elevator_Auto);
+				}
+				else{
+					pElevator->Stop();
+					Stage1Stop();
+					AirOut();
+					pIntake->Out();
+				}
+			}
 		}
 	}
 	return;
@@ -423,15 +423,15 @@ void PowerUpRobot::AutoCubeSide(){
 
 void PowerUpRobot::AutoCubeCenter(){
 	if(loopCount<Time_Elevator){
-				Stage1Start();
-				pElevator->Up1(Speed_Elevator_Auto);
+		Stage1Start();
+		pElevator->Up1(Speed_Elevator_Auto);
 	}
-			else{
-				pElevator->Stop();
-				Stage1Stop();
-				AirOut();
-				pIntake->Out();
-			}
+	else{
+		pElevator->Stop();
+		Stage1Stop();
+		AirOut();
+		pIntake->Out();
+	}
 	return;
 }
 
@@ -494,19 +494,19 @@ void PowerUpRobot::SolenoidUpdate(){
 void   PowerUpRobot::RunElevator(){
 	//tolerance for joystick
 	if(LeftY > .3 || LeftY < -.3){
-			    pElevator->Up1(-LeftY);
+			pElevator->Up1(-LeftY);
 		}
 	else{
-				pElevator->Stop();
-				Stage1Stop();
+			pElevator->Stop();
+			Stage1Stop();
 	}
 	//Stage 2:
 	if(RightY > .3 || RightY < -.3){
-				pElevator->Up2(-RightY);
+			pElevator->Up2(-RightY);
 			}
 	else{
-				pElevator->Stop2();
-				Stage2Stop();
+			pElevator->Stop2();
+			Stage2Stop();
 		}
 	StageUpdate();
 
@@ -544,12 +544,12 @@ void PowerUpRobot::StageUpdate(){
 
 void PowerUpRobot::Delay(){
 	if(DelayCounter<Time_Elevator_Delay){
-		DelayCounter=0;
-		Delayed = true;
+		DelayCounter	   =  0;
+		Delayed 	   =  true;
 	}
 	else{
-		DelayCounter=0;
-		Delayed = false;
+		DelayCounter	   =  0;
+		Delayed 	   =  false;
 	}
 }
 
