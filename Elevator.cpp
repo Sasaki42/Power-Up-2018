74b29405820ef3940bd5aac21647e2c10248d455
Sasaki42 <tylersasaki42@gmail.com>
2018-02-24 13:14:43 -0800

    Cleaning Code
    The code was cleaned. Obsolete comments were deleted and code was reorganized to be more readable

--- Elevator.cpp
+++ Elevator.cpp
@@ -2,7 +2,7 @@
  * Elevator.cpp
  *
  *  Created on: Jan 26, 2018
- *      Author: srvro
+ *      Author: Zach
  */
 
 #include "Elevator.h"
@@ -13,25 +13,20 @@ Elevator::Elevator(uint Elevator_Motor1_CH, uint Elevator_Motor2_CH) {
 	pElvMotor2 = new Spark(Elevator_Motor2_CH);
 }
 Elevator::~Elevator() {
-	// TODO Auto-generated destructor stub
+
 }
+
 void Elevator::Up1(float speed){
-	//pElvMotor1->Set(speed);
 	if(speed>0){
-	pElvMotor2->Set(Motor_Speed_Up);
+	pElvMotor1->Set(Motor_Speed_Up);
 	}
 	else{
-	pElvMotor2->Set(Motor_Speed_Down);
+	pElvMotor1->Set(Motor_Speed_Down);
 	}
-	}
-/*
-void Elevator::Down1(float speed){
-	pElvMotor1->Set(speed);
+
 }
-*/
 
 void Elevator::Up2(float speed){
-	//pElvMotor1->Set(speed);
 
 	if(speed>0){
 	pElvMotor2->Set(Motor_Speed_Up);
@@ -40,16 +35,7 @@ void Elevator::Up2(float speed){
 	pElvMotor2->Set(Motor_Speed_Down);
 	}
 }
-/*
-void Elevator::Down2(float speed){
-	pElvMotor2->Set(speed);
-}*/
-/*
-void Elevator::Down(float speed){
-	pElvMotor1->Set(speed);
-	pElvMotor2->Set(speed);
-}
-*/
+
 void Elevator::Stop(){
 	pElvMotor1->Set(Motor_Speed_Stop);
 }
