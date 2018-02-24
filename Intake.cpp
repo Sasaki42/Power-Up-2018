2fb6227965d5b6702b944ffc9e228420de9c2025
Sasaki42 <tylersasaki42@gmail.com>
2018-02-24 13:19:46 -0800

    Cleaning Code
    Cleaning up the code. All obsolete comments were deleted and the code was made more readable

--- Intake.cpp
+++ Intake.cpp
@@ -2,7 +2,7 @@
  * Intake.cpp
  *
  *  Created on: Jan 24, 2018
- *      Author: srvro
+ *      Author: Zach
  */
 
 #include "Intake.h"
@@ -10,21 +10,18 @@
 
 Intake::Intake(uint IntakeMotor1, uint IntakeMotor2)
 {
-	// TODO Auto-generated constructor stub
 	MotorIntake1		= new Spark(IntakeMotor1);
 	MotorIntake2		= new Spark(IntakeMotor2);
 }
 
 Intake::~Intake() {
-	// TODO Auto-generated destructor stub
+
 }
 
 void Intake::In(){
 		MotorIntake1->Set(Motor_Speed_Intake);
 		MotorIntake2->Set(Motor_Speed_Intake);
 }
-
-
 
 void Intake::Out(){
 	MotorIntake1->Set(Motor_Speed_Outtake);
